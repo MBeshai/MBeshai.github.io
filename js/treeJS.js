cd0e48c67c04430dd8430910b54b729cb6eaab7b
MBeshai <beshai1@umbc.edu>
2017-06-27 22:43:23 -0400

    Update treeJS.js

--- js/treeJS.js
+++ js/treeJS.js
@@ -1,4 +1,6 @@
-var svg = d3.select("svg"),
+var tree = {
+  draw: function(){
+      var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
@@ -70,3 +72,4 @@ function dragended(d) {
   d.fx = null;
   d.fy = null;
 }
+  }};

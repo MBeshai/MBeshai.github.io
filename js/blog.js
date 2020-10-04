82365bd51e24d4d21f48689df9a869111a191761
MBeshai <beshai1@umbc.edu>
2020-10-04 17:20:43 -0400

    blog post

--- js/blog.js
+++ js/blog.js
@@ -9,17 +9,33 @@
 //
 // console.log(JSON.stringify(blogPosts));
 
-function loadPost() {
+function loadPost(el){
+    var parentEl = el.parentElement;
+    var thisSection = parentEl.parentElement.parentElement.parentElement;
     document.getElementById("masthead").hidden=true;
-    document.getElementById("blogbody").classList.remove('d-none');
-    document.getElementById("articleback").classList.remove("d-none");
+    parentEl.querySelector(".blogbody").classList.remove('d-none');
+    parentEl.querySelector(".articleback").classList.remove("d-none");
+    thisSection.id = "activeSection";
+    var sections = document.getElementsByTagName('section');
+
+    for(var i = 0; i < sections.length; ++i) {
+        if(sections[i].id != "activeSection") {
+            sections[i].classList.add("d-none");
+        }
+    }
 }
 
-function bloglist() {
-    console.log("bloglist")
-    document.getElementById("masthead").removeAttribute("hidden")
-    document.getElementById("blogbody").classList.add('d-none');
-    document.getElementById("articleback").classList.add("d-none");
-    console.log("bloglist2")
+function bloglist(el) {
+    var sectionEl = el.parentElement.parentElement.parentElement.parentElement.parentElement;
+    console.log(sectionEl.classList.toString())
+    var activeSection = document.getElementById("activeSection")
+    sectionEl.querySelector(".blogbody").classList.add('d-none');
+    sectionEl.querySelector(".articleback").classList.add("d-none");
+    activeSection.id='';
+    document.getElementById("masthead").removeAttribute("hidden");
+    var sections = document.getElementsByTagName('section');
+    for(var i = 0; i < sections.length; ++i) {
+            sections[i].classList.remove("d-none");
+    }
 
 }
